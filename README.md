840a784db2c5097d152cca0dc751e94d8087b9b5
Matrix <matrix.w.chan@gmail.com>
2024-10-01 22:08:41 +0300

    add dist

---
+++ README.md
@@ -0,0 +1,1 @@
+https://helsinki-fullstackopen-part3-backend.onrender.com
--- index.js
+++ index.js
@@ -32,6 +32,8 @@ const generateId = () => {
 };
 
 app.use(express.json());
+app.use(express.static('dist'));
+
 morgan.token('postData', (req) => {
   if (req.method === 'POST') {
     return JSON.stringify(req.body);
