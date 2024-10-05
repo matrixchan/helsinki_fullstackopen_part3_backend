0f71ce0a30da3337cc03d932488e8c17aea531bd
Matrix <matrix.w.chan@gmail.com>
2024-10-05 20:36:45 +0300

    Exercise 3.22 done

--- index.js
+++ index.js
@@ -1,175 +1,169 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
+const express = require('express')
+const app = express()
+require('dotenv').config()
 
-const Person = require('./models/person');
+const Person = require('./models/person')
 
-app.use(express.json());
-app.use(express.static('dist'));
+app.use(express.json())
+app.use(express.static('dist'))
 
-const cors = require('cors');
-app.use(cors());
+const cors = require('cors')
+app.use(cors())
 
-const morgan = require('morgan');
+const morgan = require('morgan')
 
 app.use(
   morgan(
-    ':method :url :status :res[content-length] - :response-time ms :postData'
-  )
-);
+    ':method :url :status :res[content-length] - :response-time ms :postData',
+  ),
+)
 
 morgan.token('postData', (req) => {
   if (req.method === 'POST') {
-    return JSON.stringify(req.body);
+    return JSON.stringify(req.body)
   }
-  return '-';
-});
+  return '-'
+})
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:  ', request.path);
-  console.log('Body:  ', request.body);
-  console.log('---');
-  next();
-};
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-app.use(requestLogger);
+app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
-  response.send('<h1>Phonebook API</h1>');
-});
+  response.send('<h1>Phonebook API</h1>')
+})
 
 app.get('/info', async (request, response, next) => {
   try {
-    let currentDate = new Date();
-    const count = await Person.countDocuments({});
+    let currentDate = new Date()
+    const count = await Person.countDocuments({})
 
     const info = `
       Phonebook has info for ${count} people
       <br/><br/>
       ${currentDate}
-    `;
+    `
 
-    response.send(info.trim());
+    response.send(info.trim())
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 app.get('/api/persons', (request, reponse, next) => {
   Person.find({})
     .then((persons) => {
-      reponse.json(persons);
+      reponse.json(persons)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
-  const id = request.params.id;
+  const id = request.params.id
   Person.findById(id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.delete('/api/persons/:id', async (request, response, next) => {
   try {
-    const id = request.params.id;
-    const result = await Person.findByIdAndDelete(id);
+    const id = request.params.id
+    const result = await Person.findByIdAndDelete(id)
 
     if (result) {
-      response.status(204).end();
+      response.status(204).end()
     } else {
-      response.status(404).json({ error: 'Person not found' });
+      response.status(404).json({ error: 'Person not found' })
     }
   } catch (error) {
-    console.error('Error deleting person:', error);
-    // response
-    //   .status(500)
-    //   .json({ error: 'An error occurred while deleting the person' });
-    next(error);
+    next(error)
   }
-});
+})
 
 app.post('/api/persons', async (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   if (!body.name || !body.number) {
-    return response
-      .status(400)
-      .json({ error: 'The name or number is missing' });
+    return response.status(400).json({ error: 'The name or number is missing' })
   }
 
   try {
-    const existPerson = await Person.findOne({ name: body.name });
+    const existPerson = await Person.findOne({ name: body.name })
     if (existPerson) {
-      return response.status(400).json({ error: 'Name must be unique' });
+      return response.status(400).json({ error: 'Name must be unique' })
     }
 
     const person = new Person({
       name: body.name,
       number: body.number,
-    });
+    })
 
-    const savedPerson = await person.save();
-    response.json(savedPerson);
+    const savedPerson = await person.save()
+    response.json(savedPerson)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 app.put('/api/persons/:id', async (request, response, next) => {
-  const { id } = request.params;
-  const { name, number } = request.body;
+  const { id } = request.params
+  const { name, number } = request.body
 
   if (!name || !number) {
     return response.status(400).json({
       error: 'Name and number are required',
-    });
+    })
   }
 
   try {
     const updatedPerson = await Person.findByIdAndUpdate(
       id,
       { name, number },
-      { new: true, runValidators: true, context: 'query' }
-    );
+      { new: true, runValidators: true, context: 'query' },
+    )
 
     if (updatedPerson) {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     } else {
-      response.status(404).json({ error: 'Person not found' });
+      response.status(404).json({ error: 'Person not found' })
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
-app.use(unknownEndpoint);
+})
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = 3001;
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
